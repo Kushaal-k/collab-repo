2c7045ea17a88c356e8b0b593ecce97827319a1a
Kushaal Kankane <kushaalkankane12345@gmail.com>
2025-08-07 10:02:11 +0000

    [Kushaal] Fixed Error

--- Calculator.java
+++ Calculator.java
@@ -5,11 +5,15 @@ public class Calculator{
     {
         return a+b;
     }
-
+    
+    public static int subtract(int a,int b)
+    {
+        return a-b;
+    }
  
     public static int mul(int a , int b){
-
         return a*b;
+    }
 
     public static float divide(int a, int b){
         if(b==0){
@@ -17,6 +21,7 @@ public class Calculator{
         }
         return a/b;
     }
+
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
